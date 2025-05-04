62e1fbb1c9294ed20a6841777e0815b469682b7d
S Chiang <63751262+gosaurus@users.noreply.github.com>
2025-05-05 00:10:34 +0100

    Partially created radio button selection for form type

---
+++ ui/src/components/forms/BookSearchByISBN/BookSearchByISBN.tsx
@@ -0,0 +1,104 @@
+
+import { useState } from "react";
+import { bookSearchParameters } from "../../../models/bookModels";
+import { openLibrarySearchAPI } from "../../../utils/apiClients";
+import { BookSearchResults} from "../BookSearchResults/BookSearchResults";
+import { RotatingLines } from "react-loader-spinner";
+import "./BookSearchForm.scss";
+
+function BookSearchByISBN() {
+	const [formData, setFormData] = useState<bookSearchParameters>({
+    keywords: "",
+    authorName: "",
+    title: "",
+    isbn: "",
+    language: ""
+    });
+	
+  const [responseData, setResponseData] = useState();
+	const [error, setError] = useState("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+	const handleChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		const { name, value } = event.target;
+    console.log(`Name: ${name}, Value: ${value}`);
+		setFormData({...formData, [name]: value})
+	};
+
+	const handleSubmit = async (
+		event: React.FormEvent
+	): Promise<void> => {
+		event.preventDefault();
+    setLoading(true);
+		try {
+			const response = await openLibrarySearchAPI(formData);
+      setResponseData(response);
+      setError("");
+		} catch (error: unknown
+		) {
+			if (error instanceof Error)
+			setError(
+				`${error.message}`
+			)
+			else 
+				setError("An error has occurred. Contact the administrator.");
+		} finally {
+      setLoading(false);
+    }
+	};
+
+  const noQueryString = "No search parameters given"
+  const queryDisplay = Object
+    .entries(formData)
+    .map(([key, value]) => `${key}: ${value}`)
+    .join(", ");
+
+	return (
+		<>
+			<div className="book-search-form-container">
+				<h2>Book Search Form</h2>
+				<p>
+          ISBNs are unique numbers used to identify books.
+          Input ISBNs with 10 or 13 characters below to search.
+				</p>
+				<form onSubmit={handleSubmit}>
+          <div className="field">
+						<label htmlFor="isbn">
+							Search by ISBN:
+						</label>
+						<input
+              type="text"
+              name="isbn"
+              value={formData.isbn}
+              onChange={handleChange}
+            >
+            </input>
+					</div>
+					<button type="submit">Search</button>
+				</form>
+        <div className="results-wrapper">
+          {loading && 
+            (<RotatingLines 
+              visible={true}
+              width="50"
+            />)
+          }
+          {error.length > 0 && (<p>{error}</p>)}
+          {(responseData && <p>Results for parameters: {queryDisplay}</p>) ||
+            (responseData && noQueryString)}
+          {responseData &&
+            (<BookSearchResults
+              responseProps={responseData}
+              queryProps={formData} 
+            />)
+          }
+        </div>
+			</div>
+		</>
+	)
+
+}
+
+export default BookSearchForm;
--- ui/src/components/pages/BookSearchPage/BookSearchPage.tsx
+++ ui/src/components/pages/BookSearchPage/BookSearchPage.tsx
@@ -1,9 +1,65 @@
+import { useState } from "react";
 import BookSearchForm from "../../forms/BookSearchForm/BookSearchForm";
+import { bookSearchParameters } from "../../../models/bookModels";
+import BookSearchByISBN from "../../forms/BookSearchByISBN/BookSearchByISBN";
 
 export const BookSearchPage = () => {
+
+  const [radioValue, setRadioValue] = useState("");
+  const handleFormSelection = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRadioValue(event.target.value);
+    console.log(`Search selection`)
+  }
+
   return (
     <div>
-      <BookSearchForm />
+      <form>
+        <fieldset>
+          <legend>Select the book search form</legend>
+          <input 
+            type="radio" 
+            id="keywordSearch"
+            name="searchSelection"
+            value="keyword"
+            checked={radioValue==="keyword"}
+            onChange={handleFormSelection} 
+          />
+          <label htmlFor="keywordSearch">Keyword</label>
+          <input 
+            type="radio" 
+            id="authorNameSearch"
+            name="searchSelection"
+            value="authorName"
+            checked={radioValue==="authorName"}
+            onChange={handleFormSelection} 
+          />
+          <label htmlFor="authorNameSearch">Author</label>
+          <input 
+            type="radio" 
+            id="titleSearch"
+            name="searchSelection"
+            value="title"
+            checked={radioValue==="title"}
+            onChange={handleFormSelection} 
+          />
+          <label htmlFor="titleSearch">Title</label>
+          <input 
+            type="radio" 
+            id="isbnSearch"
+            name="searchSelection"
+            value="isbn"
+            checked={radioValue==="isbn"}
+            onChange={handleFormSelection} 
+          />
+          <label htmlFor="isbnSearch">ISBN</label>
+        </fieldset>
+      </form>
+      {(radioValue === "keyword" || radioValue === "title") && <BookSearchForm />}
+      {radioValue === "author" && <p>Author search form</p>}
+      {radioValue === "isbn" && <BookSearchByISBN />}
+
+      {/* <p>Refactor radio button form as component and pass in onChange handler as function?</p> */}
+
     </div>
   )
 };
