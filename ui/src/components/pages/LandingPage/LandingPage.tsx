30796d7b11f04dc9a180cd333d8a9eec83d6fd2b
gosaurus <63751262+gosaurus@users.noreply.github.com>
2025-06-14 15:20:40 +0100

    Corrected flex wrap

--- ui/src/components/pages/LandingPage/LandingPage.tsx
+++ ui/src/components/pages/LandingPage/LandingPage.tsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router";
+
 export const LandingPage = () => {
   return (
     <>
       <p>
         Landing page
+      </p>
+      <p>
+        <Link to="/BookSearch">Book Search</Link>
       </p>
     </>
   )
